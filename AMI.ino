f489379f4c99972f1c9e5c72d9489e7c17d7c899
XfableX <lachlan.maxwell@carey.com.au>
2018-05-07 17:48:48 +1000

    Added Room light calibration
    See title

--- AMI.ino
+++ AMI.ino
@@ -1,7 +1,12 @@
 #include <Esplora.h>;
+int doAvg = 0;
+  int index = 0;
+  float TotalLight=0;
+  float averageLight = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
 }
 
 void loop() {
@@ -10,10 +15,20 @@ void loop() {
   int btn2 = Esplora.readButton(2);
   int btn3 = Esplora.readButton(3);
   int btn4 = Esplora.readButton(4);
-  int value = readLightSensor();
-  Serial.println(value);
+  int value = Esplora.readLightSensor();
+  
+  if (doAvg < 1000){
+    index ++;
+    TotalLight=TotalLight+value;
+    averageLight=TotalLight/index;
+    doAvg++;
+  }
+  Serial.println(averageLight);
   if (btn1 == LOW){
-    Esplora.writeRGB(250,0,0);
+    index = 0;
+    TotalLight=0;
+    averageLight=0;
+    doAvg = 0;
   }
   else if (btn2 == LOW){
     Esplora.writeRGB(0,250,0);
