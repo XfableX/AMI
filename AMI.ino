8456d4759c66e016d85a99ea8387bd7e65464f8f
XfableX <lachlan.maxwell@carey.com.au>
2018-05-07 10:03:52 +1000

    Made AMI go RGB and added Doc file
    lol

--- AMI.ino
+++ AMI.ino
@@ -1,9 +1,28 @@
+#include <Esplora.h>;
 void setup() {
   // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int btn1 = Esplora.readButton(1);
+  int btn2 = Esplora.readButton(2);
+  int btn3 = Esplora.readButton(3);
+  int btn4 = Esplora.readButton(4);
 
+
+  if (btn1 == LOW){
+    Esplora.writeRGB(250,0,0);
+  }
+  else if (btn2 == LOW){
+    Esplora.writeRGB(0,250,0);
+    }
+   else if (btn3 == LOW) {
+    Esplora.writeRGB(0,0,255);
+   }
+   else if (btn4 == LOW) {
+    Esplora.writeRGB(255,255,255);
+    
+   }
 }
