6da400da9d506838e6cdbb88eead4aa9102339dc
XfableX <lachlan.maxwell@carey.com.au>
2018-05-07 22:22:58 +1000

    Added basic light reading, inaccurate however

--- AMI.ino
+++ AMI.ino
@@ -1,8 +1,14 @@
 #include <Esplora.h>;
+bool AvgE=false;
+int value;
 int doAvg = 0;
-  int index = 0;
-  float TotalLight=0;
-  float averageLight = 0;
+int index = 0;
+float TotalLight=0;
+float averageLight = 0;
+int secondsActv;
+char AsciiToMorse[37]={
+  1,2,3,4 };
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -10,34 +16,68 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   int btn1 = Esplora.readButton(1);
   int btn2 = Esplora.readButton(2);
   int btn3 = Esplora.readButton(3);
   int btn4 = Esplora.readButton(4);
-  int value = Esplora.readLightSensor();
+  
+  value = Esplora.readLightSensor();
   
   if (doAvg < 1000){
     index ++;
     TotalLight=TotalLight+value;
     averageLight=TotalLight/index;
     doAvg++;
+    //Serial.println(averageLight);
   }
-  Serial.println(averageLight);
+  else if (doAvg == 1000){
+    String strn = ("The average light level in this room is: ");
+    Serial.println(strn);
+    Serial.println(averageLight);
+    AvgE=true;
+    doAvg++;
+  //  blah();
+  }
+  
+  
   if (btn1 == LOW){
-    index = 0;
+    index = 1;
     TotalLight=0;
     averageLight=0;
     doAvg = 0;
   }
+  
   else if (btn2 == LOW){
-    Esplora.writeRGB(0,250,0);
+    bool active = false;
+    if (active ==false){
+    active = true;
+    Esplora.writeRGB(0,255,0);
+    delay(100);
+    Esplora.writeRGB(0,0,0);
+    delay(200);
+    active = false;
+    }
     }
    else if (btn3 == LOW) {
-    Esplora.writeRGB(0,0,255);
+    Esplora.writeRGB(0,255,0);
+    delay(750);
+    Esplora.writeRGB(0,0,0);
    }
    else if (btn4 == LOW) {
     Esplora.writeRGB(255,255,255);
     
    }
+    if (value > averageLight+400&&AvgE==true){
+      blah();
+    }
 }
+
+void blah(){
+   
+   secondsActv = (millis()/1000);
+   Serial.println(value);
+   Serial.println(secondsActv);
+}
+
+
